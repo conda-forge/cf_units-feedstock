9211d15a9735f6c383983d377bc0b68fa777c910
Bill Little <bill.little@metoffice.gov.uk>
2021-08-04 22:54:38 +0100

    Update bld.bat
    Use `LIBRARY_PREFIX` rather than `PREFIX` for `windows`

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
-set UDUNITS2_INCDIR="%PREFIX%\include\udunits2"
-set UDUNITS2_XML_PATH="%PREFIX%\share\udunits\udunits2.xml"
+set UDUNITS2_INCDIR="%LIBRARY_PREFIX%\include\udunits2"
+set UDUNITS2_XML_PATH="%LIBRARY_PREFIX%\share\udunits\udunits2.xml"
 
 "%PYTHON%" -m pip install . --no-deps -vv
 if errorlevel 1 exit 1

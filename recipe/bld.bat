cf0ed12469237ba53299bf9cb9f8107e46d77778
Bill Little <bill.little@metoffice.gov.uk>
2021-08-05 09:07:03 +0100

    Update bld.bat
    Add .bat share directory diagnostics

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,9 @@
 set UDUNITS2_INCDIR="%LIBRARY_PREFIX%\include\udunits2"
 set UDUNITS2_XML_PATH="%LIBRARY_PREFIX%\share\udunits\udunits2.xml"
 
+echo on
+dir %LIBRARY_PREFIX%\share\udunits
+@echo off
+
 "%PYTHON%" -m pip install . --no-deps -vv
 if errorlevel 1 exit 1
